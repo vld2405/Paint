5b796e970dd1b9494b93239bb2ea8beb34d2f6de
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2026-03-29 17:25:47 +0300

    tema 7 init

---
+++ tema7/BSpline/BSpline/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BSpline
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
